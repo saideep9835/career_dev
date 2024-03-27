7b05b615dba19a2c34204f6baeb598b61fabd7f4
saideep9835 <tsaideep98@gmail.com>
2024-03-26 19:06:20 -0500

    Removed spacing in main file

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from  database.routes.route import router
 from database.config.config_data import client
 from pymongo.mongo_client import MongoClient
-
-
 import sys
 
 app = FastAPI()
-
-
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins (replace with specific origins as needed)
@@ -19,9 +13,6 @@ app.add_middleware(
     allow_methods=["*"],   # Allow all methods
     allow_headers=["*"],   # Allow all headers
 )
-
-
-
 app.include_router(router)
 
 # # @app.get("/")
